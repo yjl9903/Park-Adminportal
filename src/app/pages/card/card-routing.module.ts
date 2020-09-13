14028919af22dcf7a81de2f29a0e3db0412fc697
XLor <yjl9903@vip.qq.com>
2020-09-14 01:47:47 +0800

    add: card structure

--- src/app/pages/card/card-routing.module.ts
+++ src/app/pages/card/card-routing.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CardComponent } from './card.component';
+import { CreateCardComponent } from './create-card/create-card.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: CardComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'create',
+      },
+      {
+        path: 'create',
+        pathMatch: 'full',
+        component: CreateCardComponent,
+      },
+      {
+        path: 'list',
+        pathMatch: 'full',
+        component: CardListComponent,
+      },
+    ],
   },
 ];
 
--- src/app/pages/card/card.module.ts
+++ src/app/pages/card/card.module.ts
@@ -4,9 +4,11 @@ import { NzLayoutModule, NzMenuModule } from 'ng-zorro-antd';
 
 import { CardComponent } from './card.component';
 import { CardRoutingModule } from './card-routing.module';
+import { CreateCardComponent } from './create-card/create-card.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 @NgModule({
-  declarations: [CardComponent],
+  declarations: [CardComponent, CreateCardComponent, CardListComponent],
   imports: [CommonModule, CardRoutingModule, NzLayoutModule, NzMenuModule],
 })
 export class CardModule {}
