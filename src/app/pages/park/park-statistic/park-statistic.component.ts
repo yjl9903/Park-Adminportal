1a42294477599fab671c6ec29dcfe6fc3fa59d49
XLor <yjl9903@vip.qq.com>
2020-09-16 15:57:46 +0800

    update: statistic number

--- src/app/pages/park/park-statistic/park-statistic.component.ts
+++ src/app/pages/park/park-statistic/park-statistic.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ParkService } from '../../../service/park.service';
+import { CardService } from '../../../service/card.service';
 
 @Component({
   selector: 'app-park-statistic',
   templateUrl: './park-statistic.component.html',
   styleUrls: ['./park-statistic.component.css'],
 })
-export class ParkStatisticComponent implements OnInit {
-  constructor() {}
+export class ParkStatisticComponent implements OnInit, OnDestroy {
+  subStatus: Subscription;
+  subInfo: Subscription;
 
-  ngOnInit(): void {}
+  parkStatusNum = 0;
+  parkInfoNum = 0;
+  cardNum = 0;
+
+  constructor(
+    private cardService: CardService,
+    private parkService: ParkService
+  ) {
+    this.cardService.getAllCards().subscribe((cards) => {
+      this.cardNum = cards.length;
+    });
+  }
+
+  ngOnInit(): void {
+    this.subStatus = this.parkService.parkStatusSub.subscribe((statuses) => {
+      this.parkInfoNum -= this.parkStatusNum;
+      this.parkStatusNum = statuses.length;
+      this.parkInfoNum += this.parkStatusNum;
+    });
+
+    this.subInfo = this.parkService.parkInfoSub.subscribe(() => {
+      this.parkInfoNum++;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subStatus.unsubscribe();
+    this.subInfo.unsubscribe();
+  }
 }
